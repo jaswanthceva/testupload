3c5f3fe528bcfcadbcb661c77c88a67c456439d4
HP <HP@JC-DESK>
2022-07-03 06:45:54 +0530

    First Commit

---
+++ testspring/src/main/java/com/jclearn/testspring/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.jclearn.testspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/hi")
+	public String welcome(){
+		
+		return "THIS IS STARTING PAGE";
+	}
+
+}
